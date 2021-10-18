5f194adab9f9c47349c054901e27c884b8ad79ba
odysseus443 <81030083+odysseus443@users.noreply.github.com>
2021-10-18 18:42:09 +0800

    Added detection for new modules.

--- autosign.py
+++ autosign.py
@@ -3,18 +3,18 @@
 # details: https://www.gnu.org/licenses/agpl-3.0.en.html
 # Modified by odysseus443
 
-# Kernel sign script path
-sign_script_path = '/usr/src/kernels/{uname_release}/scripts/sign-file'
-# Common kernel path
-path_common = '/lib/modules/'
-# main.py directory path
-main_path = '/var/log/autosign/autosign.py'
-
-# Do not change below this line #
-
 import os
 import datetime
 
+# Kernel sign script path
+# Common kernel path
+path_common = '/lib/modules/'
+# Key location
+public_key = '/etc/pki/tls/mok/mok.der'
+private_key = '/etc/pki/tls/mok/mok.key'
+
+
+_date = datetime.datetime.now().strftime("%Y%m%d")
 
 class MOKKeyError(Exception):
 	""" Machine Owner Key not found in the keys directory """
@@ -25,10 +25,27 @@ class SignError(Exception):
 	""" Error while signing the kernel modules """
 	pass
 
-_date = datetime.datetime.now().strftime("%Y%m%d")
+
+def sign(kernel_modules, kernel):
+	for i in kernel_modules:
+		print('Signing ' + i)
+		sign_script_path = '/usr/src/kernels/{uname_release}/scripts/sign-file'
+		sign_script_path = sign_script_path.format(uname_release=kernel)
+		run_script = sign_script_path + ' sha256 ' + private_key + ' ' + public_key + ' ' + i
+		try:
+			os.system(run_script)
+			print('Signed ' + i)
+			with open(f'/var/log/autosigner.log', 'a+') as f:
+				f.write('Signed ' + i + '\n')
+		except Exception as e:
+			print('FAILURE: ' + i)
+			print(e)
+			with open(f'/var/log/autosigner.log', 'a+') as f:
+				f.write(e + '\n')
+				f.write(datetime.datetime.now().strftime('%c') + '\n')
+			raise SignError
 
 def main():
-	global sign_script_path
 	# Get current kernel info
 	kernel_current = os.uname().release
 
@@ -37,50 +54,48 @@ def main():
 	# Sort kernels alphabetically
 	kernels_present.sort()
 	# Only need to proceed if there's been a kernel update
-	if (kernel_current != kernels_present[-1]):
-		kernel_updated = kernels_present[-1]
-		print('Updated kernel found: ' + kernel_updated)
-		kernel_path = path_common + kernel_updated + '/'
-		module_path = kernel_path + 'extra'
-		kernel_modules = []
-		for root, dirs, files in os.walk(module_path):
-			for file in files:
-				kernel_modules.append(os.path.join(root, file))
+	kernel_updated = kernels_present[-1]
+	kernel_path = path_common + kernel_updated + '/'
+	module_path = kernel_path + 'extra'
+	kernel_modules = []
+	added_modules = []
+	with open('/etc/autosign.conf', 'a+') as f:
+		config = f.readlines()
+	kernel_modules_check = [i.replace('\n','') for i in config]
+	for root, dirs, files in os.walk(module_path):
+		for file in files:
+			kernel_modules.append(os.path.join(root, file))
+	if kernel_modules_check != kernel_modules:
+		with open ('/etc/autosign.conf', 'a+') as fnew:
+			for i in kernel_modules:
+				if i not in kernel_modules_check and i != '':
+					fnew.write(f'\n{i}')
+					added_modules.append(i)
+		with open(f'/var/log/autosigner.log', 'a+') as f:
+			for i in added_modules:
+				f.write(f'Found added module: {i}' + datetime.datetime.now().strftime('%c') + '\n')
+		sign(added_modules, kernel_updated)
+		return
+	elif kernel_current != kernels_present[-1]:
 		# Check if keys exist
 		keys = [f.name for f in os.scandir('/etc/pki/tls/mok/') if f.is_file()]
 		if ('mok.key' in keys) and ('mok.der' in keys):
-			print('Keys found in keys directory.')
-			public_key = '/etc/pki/tls/mok/mok.der'
-			private_key = '/etc/pki/tls/mok/mok.key'
-			sign_script_path = sign_script_path.format(uname_release=kernel_updated)
-			for i in kernel_modules:
-				print('Signing ' + i)
-				run_script = sign_script_path + ' sha256 ' + private_key + ' ' + public_key + ' ' + i
-				try:
-					os.system(run_script)
-					print('Signed ' + i)
-					with open(f'/var/log/autosigner-{_date}.log', 'a+') as f:
-						f.write('Signed ' + i + '\n')
-				except Exception as e:
-					print('FAILURE: ' + i)
-					print(e)
-					with open(f'/var/log/autosigner-{_date}.log', 'a+') as f:
-						f.write(e + '\n')
-						f.write(datetime.datetime.now().strftime('%c') + '\n')
-					raise SignError
+			sign(kernel_modules, kernel_updated)
 		else:
 			print('Keys NOT FOUND')
-			with open(f'/var/log/autosigner-{_date}.log', 'a+') as f:
+			with open(f'/var/log/autosigner.log', 'a+') as f:
 				f.write('Keys NOT FOUND. ' + datetime.datetime.now().strftime('%c') + '\n')
 			raise MOKKeyError
+		return
 	else:
 		print('Kernel not updated, signing new kernels not required.')
-		with open(f'/var/log/autosigner-{_date}.log', 'a+') as f:
+		with open(f'/var/log/autosigner.log', 'a+') as f:
 			f.write('Kernel not updated, signing new kernels not required. ' + datetime.datetime.now().strftime('%c') + '\n')
+		return
 
 
 if __name__ == '__main__':
-	with open(f'/var/log/autosigner-{_date}.log', 'a+') as f:
+	with open(f'/var/log/autosigner.log', 'a+') as f:
 		f.write('Service ran at ' + datetime.datetime.now().strftime('%c') + '\n')
 
 	main()
